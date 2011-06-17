dd72fda5e6db4b04fb515cfa2c4994615a67ce5b
joseph <joe@cs.brown.edu>
2011-06-16 23:52:56 -0700

    mac

--- tests/machineTest.js
+++ tests/machineTest.js
@@ -1,22 +1,46 @@
+function expectEqual(val, testVal) {
+    if(val !== testVal) { 
+        print("!!!Failed: " + String(val) + " !== " + String(testVal));
+    }
+}
+
+function expect(val) {
+    if(val) { expectEqual(true, true); }
+}
+
 function testMachine() {
-    print("new Machine(10, 255, 1000);");
     var m = new Machine(10, 255, 1000);
 
     print("Should have 10 as a valid slot");
-    if(m.validSlot(10)) { print("Yes"); }
-    else { print("Failed"); }
+    expect(m.validSlot(10));
 
     print("Should have 0 as a valid slot");
-    if(m.validSlot(0)) { print("Yes"); }
-    else { print("Failed"); }
+    expect(m.validSlot(0));
 
     print("Should have 11 as an invalid slot");
-    if(m.validSlot(11)) { print("Failed"); }
-    else { print("Yes"); }
+    expect(m.validSlot(11));
 
     print("Should have -1 as an invalid slot");
-    if(m.validSlot(-1)) { print("Failed"); }
-    else { print("Yes"); }
+    expect(m.validSlot(-1));
+
+    print("First slot should contain ident");
+    var slot0 = m.getProponentSlot(0);
+    expect(slot0.field(52) === 52);
+    
 }
 
 testMachine();
+
+function testIdent(m) {
+    var o = {};
+    print("Ident should make an ident");
+    expectEqual(Ident.make(m)(o), o);
+}
+
+function testZero(m) {
+    print("Zero should make to zero");
+    expectEqual(Zero.make(m), 0);
+}
+
+testIdent();
+testZero();
